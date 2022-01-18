34f246e369ac28d70ee072e0d1b0cfe5696f199a
ember <aliomarov.522@mail.ru>
2022-01-18 19:47:59 +0300

    add lesson 2.2

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -4,6 +4,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.timezone import now
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(
@@ -12,6 +15,7 @@ class ShopUser(AbstractUser):
     )
     age = models.PositiveIntegerField(
         verbose_name='возраст',
+        default=18,
     )
     is_deleted = models.BooleanField(
         default=False,
@@ -31,3 +35,49 @@ class ShopUser(AbstractUser):
             return False
         else:
             return True
+
+
+class ShopUserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'W'
+
+    GENDER_CHOICES = (
+        (MALE, 'М'),
+        (FEMALE, 'Ж'),
+    )
+
+    user = models.OneToOneField(
+        ShopUser,
+        unique=True,
+        null=False,
+        db_index=True,
+        on_delete=models.CASCADE,
+    )
+
+    tagline = models.CharField(
+        verbose_name='тэги',
+        max_length=128,
+        blank=True,
+    )
+
+    about_me = models.TextField(
+        verbose_name='о себе',
+        max_length=512,
+        blank=True,
+    )
+
+    gender = models.CharField(
+        verbose_name='пол',
+        max_length=1,
+        choices=GENDER_CHOICES,
+        blank=True,
+    )
+
+    @receiver(post_save, sender=ShopUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=ShopUser)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.shopuserprofile.save()
